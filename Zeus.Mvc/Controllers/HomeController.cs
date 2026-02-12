84a8825f62b8f36c81744d6b744cb721913bce78
xabier <montcouronne@hotmail.com>
2026-02-12 00:05:22 -0600

    Add project Web MVC

---
+++ Zeus.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Zeus.Mvc.Models;
+
+namespace Zeus.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
